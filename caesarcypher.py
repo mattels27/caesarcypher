31944286c3f05bcdb67a7317829db223ec91a0cd
Matheus Macedo <91211831+mattels27@users.noreply.github.com>
2023-12-28 03:51:07 -0300

    Versão final.

--- caesarcypher.py
+++ caesarcypher.py
@@ -1,121 +1,200 @@
-from random import *
 from math import *
+import random
 from sympy import mod_inverse
+import tkinter as tk
+
+#Eu sei que o tkinter é uma desgraça, mas deixa quieto.
+#O alfabeto fica ao gosto do dev. Eu fiz o meu com os 118 caracteres disponíveis no alfabeto + o espaço.
+#Não incluí o 'U' com trema. Se tentar criptografar palavras em alemão, não vai dar certo (não é o único impeditivo aliás).
+
+
 
 alphabet=['A','Á','À','Â','Ã','B','C','Ç','D','E','É','Ê','F','G','H','I','Í','J','K','L','M','N','O','Õ','Ó','Ô','P',
           'Q','R','S','T','U','Ú','Û','V','W','X','Y','Z','a','á','à','â','ã','b','c','ç','d','e','é','ê','f','g',
           'h','i','í','j','k','l','m','n','o','õ','ó','ô','p','q','r','s','t','u','ú','v','w','x','y','z','1','2','3',
           '4','5','6','7','8','9','0',' ','"','!','@','#','$','%','¨','&','*','(',')','-','_','=','+','[',']','{',
           '}','?',',','.','|','/','<','>',';',':','ª','º','§']
-menu=True
 
+#Aqui, o algorítmo de cifragem do texto fornecido pelo usuário.
 
-while menu:
-    print('Caesar Cypher')
-    print('1. Criptografar')
-    print('2. Descriptografar')
-    print('3. Gerador de Chaves')
-    print('4. Sair')
-    a=input('Digite: ')
+def encrypting_algorithm(text, variable_a, variable_b):
 
-    if a==('1'):
-        while True:
-            var_a= int(input('Escolha sua variável a: '))
-            var_b= int(input('Escolha sua variável b (entre 0 e 118): '))
+    #'line_length' fica ao critério de quem for editar. esse é o valor limite para o programa quebrar o texto.
+     
+    new_msg=''
+    current_line_length= 0
+    line_length= 20
 
-            if gcd(var_a, 119)!= 1:
-                print (' Chave inválida.')
-                break
+    #Tira o MDC da variável escolhida pelo user e o tamanho do alfabeto.
+    #gdc = Greatest Common Divisor.
 
-            msg_index=[]
-            new_msg_=[]
-            msg=input('Digite sua mensagem: ')
-
-            for k in msg:
-                msg_index.append(alphabet.index(k))
-
-            for j in msg_index:
-                x=((var_a*(j+1)+var_b)%119)-1
-                new_msg_.append(alphabet[x])
-
-            print(''.join(new_msg_))
-            print()
-            print('1. Continuar criptografando')
-            print('2. Voltar ao menu')
-            b=input('Digite: ')
-
-            if b==('1'):
-                True
-            if b==('2'):
-                break
+    if gcd(variable_a, len(alphabet))!= 1:
+        return 'Chave Inválida. Precisa ser coprimo de 119.'
     
-    if a==('2'):
-        while True:
-            var_a= int(input('Escolha sua variável a: '))
-            var_b= int(input('Escolha sua variável b (entre 0 e 118): '))
+    #Vai obter a indexação ds letras, já cifradas.
 
-            alt_key_a= mod_inverse(var_a, 119)
-            alt_key_b= 119 - int(var_b) * alt_key_a
+    for letter in text:
+        if letter == ' ':
+            new_msg += ' '
+            current_line_length += 1
+        else:
+            new_index = ((variable_a*(alphabet.index(letter) + 1) + variable_b) % len(alphabet)) - 1
+            new_msg += alphabet[new_index]
+            current_line_length += 1
+        if current_line_length == line_length:
+            new_msg += '\n'
+            current_line_length = 0
+    
+    return new_msg
 
-            msg=input('Digite sua mensagem codificada: ')
-            msg_index=[]
-            new_msg_=[]
+#O processo de decifragem de uma mensagem.
+
+def decrypting_algorithm(text, variable_a, variable_b):
+    #Nada de novo por aqui, o processo é o mesmo só que ao contrário.
+    new_msg=''
+    current_line_length= 0
+    line_length= 20
+
+    if gcd(variable_a, len(alphabet))!= 1:
+        return 'Chave Inválida. Precisa ser coprimo de 119.'
+    
+    for letter in text:
+        if letter == ' ':
+            new_msg += ' '
+            current_line_length += 1
+        else:
+            alt_key_a= mod_inverse(variable_a, len(alphabet))
+            alt_key_b= (len(alphabet) - int(variable_b)) * alt_key_a
+            new_index = ((alt_key_a*(alphabet.index(letter) + 1) + alt_key_b) % len(alphabet)) - 1
+            new_msg += alphabet[new_index]
+            current_line_length += 1
+        if current_line_length == line_length:
+            new_msg += '\n'
+            current_line_length = 0
+    
+    return new_msg
+
+#Não precisava, mas fiz um gerador de chaves baseado no tamanho do alfabeto que o usuário fornece.
+
+def key_generate_algorithm(alphabet_length):
+    a_variables=[]
+
+    for k in range(2, alphabet_length):
+        if gcd(k, alphabet_length) == 1:
+            a_variables.append(k)
             
-            for l in msg:
-                msg_index.append(alphabet.index(l))
-
+    def random_a_key_choose(a_variables):
+        return random.choice(a_variables)
             
-            for i in msg_index:
-                x=((alt_key_a *(i+1)+alt_key_b)%119)-1
-                new_msg_.append(alphabet[x])
+    def random_b_key_choose(alphabet_length):
+        return random.randint(1, alphabet_length)
 
-            print(''.join(new_msg_))
-            print()
-            print('1. Continuar descriptografando')
-            print('2. Voltar ao menu')
-            c=input('Digite: ')
+    a_key= random_a_key_choose(a_variables)
+    b_key= random_b_key_choose(alphabet_length)
 
-            if c==('1'):
-                True
-            if c==('2'):
-                break
+    return f'{a_key}x + {b_key}'
+    
+#Pra cada um dos procedimentos, tem uma função que faz o tratamento dos dados fornecidos no programa.
 
-    if a==('3'):
-        while True:
-            a= int(input('Quantos caracteres tem seu alfabeto?: '))   
-            a_variables=[]
+def encrypting_process():
+    #Esse procedimento trata a mensagem e as variáveis que o user deu. Se ele passar
+    #qualquer coisa que não for um número, o programa pede para fornecer um valor válido.
 
-            for k in range(2, a):
-               if gcd(k, a) == 1:
-                    a_variables.append(k)
-            
-            def random_a_key_choose(a_variables):
-                return random.choice(a_variables)
-            
-            def random_b_key_choose(a):
-                return random.randint(1, a)
+    try:
+        message = entry.get()
+        variable_a = int(shift_a_entry.get())
+        variable_b = int(shift_b_entry.get())
+    except ValueError:
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, "Erro: Valor inválido, por favor insira um valor válido.")
+        return
+    
+    encrypted_text = encrypting_algorithm(message, variable_a, variable_b)
+    result_text.delete(1.0, tk.END)  #Limpa o que tiver na caixa. Para colocar a nova info que o programa der.
+    result_text.insert(tk.END, encrypted_text)
 
-            a_key= random_a_key_choose(a_variables)
-            b_key= random_b_key_choose(a)
+    
+def decrypting_process():
+    #Mesma ação do processo anterior.
 
-            alt_key_a= mod_inverse(a_key, a)
-            alt_key_b= -((a - int(b_key)) * alt_key_a)*(-1)
+    try:
+        message = entry.get()
+        variable_a = int(shift_a_entry.get())
+        variable_b = int(shift_b_entry.get())
+    except ValueError:
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, "Erro: Valor inválido, por favor insira um valor válido.")
+        return
+    
+    decrypted_text = decrypting_algorithm(message, variable_a, variable_b)
+    result_text.delete(1.0, tk.END)  #Mesma coisa.
+    result_text.insert(tk.END, decrypted_text)
 
 
-            print('Sua chave aleatória é:')
-            print()
-            print(f'{a_key}x + {b_key} ')
-            print()
-            print('E sua chave para decifrar: ')
-            print()
-            print(f'{alt_key_a}x + {alt_key_b}')
-            print('1. Gerar mais chaves')
-            print('2. Voltar ao menu')
-            choice=input('Digite: ')
-            if choice==('1'):
-                True
-            if choice==('2'):
-                break
-              
-    if a==('4'):
-        break   
+def key_generate_process():
+    #Mesma coisa...
+
+    try:
+        len_alphabet = int(entry_alphabet_length.get())
+    except TypeError:
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, "Erro: Valor inválido, por favor insira um valor válido.")
+        return
+    
+    generated_key= key_generate_algorithm(len_alphabet)
+    key_text.delete(1.0, tk.END) #Mesma coisa.
+    key_text.insert(tk.END, generated_key)
+
+#Resumidamente, 'root' é a janela, com 'title' sendo o nome na barra; 'Label' são os textinhos como "Insira seu texto"; 
+#'Entry' é a caixa onde inserimos a info pedida; 'Button' são os botões em que iremos chamar o processo.
+#'mainloop' é o que vai manter a janela aberta.
+
+root = tk.Tk()
+root.title("Codificador César")
+
+label = tk.Label(root, text="Insira seu texto:")
+label.grid(row=0, column=0,padx=10, pady=10)
+
+label_key_generate = tk.Label(root, text="Insira o número de caracteres do seu alfabeto.")
+label_key_generate.grid(row=0, column=5, padx=10, pady=10)
+
+entry = tk.Entry(root, width=80)
+entry.grid(row=2, column=0, padx=10, pady=10)
+
+entry_alphabet_length = tk.Entry(root, width=80)
+entry_alphabet_length.grid(row=2, column=5, padx=10, pady=10)
+
+shift_label_a = tk.Label(root, text="Insira a variável 'a':")
+shift_label_a.grid(row=4, column=0, padx=0,pady=0)
+
+shift_label_b = tk.Label(root, text="Insira a variável 'b':")
+shift_label_b.grid(row=6, column=0, padx=0,pady=0)
+
+shift_a_entry = tk.Entry(root, width=5)
+shift_a_entry.grid(row=4, column=1, padx=0,pady=0)
+
+shift_b_entry = tk.Entry(root, width=5)
+shift_b_entry.grid(row=6, column=1, padx=0,pady=0)
+
+encrypt_button = tk.Button(root, text="Criptografar", command=encrypting_process)
+encrypt_button.grid(row=8, column=0, padx=10,pady=10)
+
+decrypt_button = tk.Button(root, text="Descriptografar", command=decrypting_process)
+decrypt_button.grid(row=9, column=0, columnspan=1, pady=10)
+
+random_key_generate_button= tk.Button(root, text="Gerar chave aleatória", command=key_generate_process)
+random_key_generate_button.grid(row=3, column=5, columnspan=1, pady=10)
+
+result_label = tk.Label(root, text="Texto cifrado:")
+result_label.grid(row=10, column=0, padx=10, pady=10)
+
+result_text = tk.Text(root, height=40, width=80)
+result_text.grid(row=14, column=0, padx=10, pady=10)
+
+result_key_label = tk.Label(root, text="Sua chave aleatória é:")
+result_key_label.grid(row=4, column=5, padx=10, pady=10)
+
+key_text = tk.Text(root, height=1, width=20)
+key_text.grid(row=5, column=5, padx=10, pady=10)
+
+root.mainloop()
